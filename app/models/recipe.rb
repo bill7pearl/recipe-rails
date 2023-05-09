afe2e73362712a6b4bdb4a502fd952886a3fb1e3
Salwa99 <s.ritage99@gmail.com>
2023-05-09 17:20:36 +0100

    Create the  public recipe index view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods, source: :food
 
+  scope :public_recipes, -> { where(public: true) }
+
   validates :name, presence: true
   validates :cooking_time, presence: true
 end
