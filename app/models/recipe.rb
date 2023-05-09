e46eb6e4eeb632b0e98b8f01c09ca30dbeed5a38
Salwa99 <s.ritage99@gmail.com>
2023-05-09 13:25:18 +0100

    Add validation to the recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods, source: :food
+
+  validates :name, presence: true
+  validates :cooking_time, presence: true
 end
