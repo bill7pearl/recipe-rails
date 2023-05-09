c64bc0c2e2b02160bae3f58996910d3e82a30adf
Salwa99 <s.ritage99@gmail.com>
2023-05-09 16:46:47 +0100

    Generate views for devise

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_commit :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
