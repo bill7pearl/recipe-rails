cc0bda7366e63e18247cd4666715ef0ca3dc5897
Salwa99 <s.ritage99@gmail.com>
2023-05-09 16:43:28 +0100

    update the aplication controller

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
