f20e65fc27cf65ebca579ca78ea5acec7f63a235
Salwa99 <s.ritage99@gmail.com>
2023-05-09 16:56:18 +0100

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  authenticated :user do
+    root 'foods#index', as: :authenticated_root
+
+    resources :foods, only: [:index, :new, :create]
+    resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+      member do
+        post 'toggle_public'
+      end
+    end
+    resources :inventories, only: [:index, :show, :new, :create, :destroy] do
+      resources :inventory_foods, only: [:create, :update, :destroy]
+    end
+    resources :recipe_foods, only: [:create, :update, :destroy]
+    resources :public_recipes, only: [:index, :show]
+    resources :shopping_lists, only: [:index]
+  end
+
+  root 'public_recipes#index'
 end
