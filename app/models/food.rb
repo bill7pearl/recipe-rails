263f067de6bbffc2dd654ac21aa19dd42af05b5e
Salwa99 <s.ritage99@gmail.com>
2023-05-09 13:29:19 +0100

    Add validation to the food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods, source: :recipe
   has_many :inventory_foods
+
+  validates :name, presence: true, length: { maximum: 50 }
 end
