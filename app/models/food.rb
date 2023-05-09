0e8d4fc34a8d65721936f38093acff33c5055cd2
Salwa99 <s.ritage99@gmail.com>
2023-05-09 13:23:01 +0100

    Generate the inventory model

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,5 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods, source: :recipe
+  has_many :inventory_foods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :foods
   has_many :recipes
+  has_many :inventories
 end
