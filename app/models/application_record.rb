ed11d78355df52484bd1e0ceafbb49bcdbd17c99
Salwa99 <s.ritage99@gmail.com>
2023-05-09 17:09:34 +0100

    Fix error

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  before_commit :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  primary_abstract_class
 end
