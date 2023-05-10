b5c927718ec32491d0d59c5198d87b828ded7d34
Salwa99 <s.ritage99@gmail.com>
2023-05-10 11:26:42 +0100

    Update the recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,45 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+
   def index
     @recipes = current_user.recipes
+    @recipes = Recipe.where(public: true)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: !@recipe.public)
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation, :cooking_time, :description, :public)
   end
 end
